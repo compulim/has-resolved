a9341d65d8350bbffb257d0d272cdf0ce57b706c
William Wong <compulim@users.noreply.github.com>
2025-12-24 08:53:47 +0000

    Fix precommit

--- packages/has-resolved/src/hasRejected.ts
+++ packages/has-resolved/src/hasRejected.ts
@@ -1,8 +1,8 @@
 export default function hasRejected(promise: Promise<unknown>): Promise<boolean> {
   const symbol = Symbol();
 
-  return Promise.race([
-    promise,
-    new Promise((_, reject) => setImmediate(() => reject(symbol)))
-  ]).then(() => false, result => result !== symbol);
+  return Promise.race([promise, new Promise((_, reject) => queueMicrotask(() => reject(symbol)))]).then(
+    () => false,
+    result => result !== symbol
+  );
 }
--- packages/has-resolved/src/hasResolved.ts
+++ packages/has-resolved/src/hasResolved.ts
@@ -1,8 +1,8 @@
 export default function hasResolved(promise: Promise<unknown>): Promise<boolean> {
   const symbol = Symbol();
 
-  return Promise.race([
-    promise,
-    new Promise(resolve => setImmediate(() => resolve(symbol)))
-  ]).then(result => result !== symbol, () => false);
+  return Promise.race([promise, new Promise(resolve => queueMicrotask(() => resolve(symbol)))]).then(
+    result => result !== symbol,
+    () => false
+  );
 }
